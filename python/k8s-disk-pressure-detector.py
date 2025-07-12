aa0df39ad54f176664d1a0dcd069a835feca7942
Rajchal <rajchalanjal1@gmail.com>
2025-07-12 21:40:58 +0545

    Kubernetes Node Disk Pressure Detector

---
+++ python/k8s-disk-pressure-detector.py
@@ -0,0 +1,13 @@
+from kubernetes import client, config
+
+def main():
+    config.load_kube_config()
+    v1 = client.CoreV1Api()
+    nodes = v1.list_node().items
+    for node in nodes:
+        for cond in node.status.conditions:
+            if cond.type == "DiskPressure" and cond.status == "True":
+                print(f"Node {node.metadata.name} has DiskPressure!")
+
+if __name__ == "__main__":
+    main()
